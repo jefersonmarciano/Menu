f2fdd6106371226e986aa9fc820b741661333124
jefersonmarciano <jefersonmarcian6@gmail.com>
2023-10-27 20:43:39 -0300

    Sua mensagem de commit aqui

--- js/app.js
+++ js/app.js
@@ -31,6 +31,7 @@ cardapio.metodos = {
             let temp = cardapio.templates.item.replace(/\${img}/g, e.img)
             .replace(/\${nome}/g, e.name)
             .replace(/\${preco}/g, e.price.toFixed(2).replace('.',','))
+            .replace(/\${id}/g, e.id)
 
             // botao ver mais foi clicado (12 itens)
             if (vermais && i >= 8 && i < 12) {
@@ -60,31 +61,55 @@ cardapio.metodos = {
 
         $("#btnVerMais").addClass('hidden');
 
+    },
+
+
+    // diminuir a quantidade do item no cardapio
+    diminuirQuantidade: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-" + id).text());
+
+        if (qntdAtual > 0) {
+            $("#qntd-" + id).text(qntdAtual - 1)
+        }
+
+    },
+
+    // aumentar a quantidade do item no cardapio
+    aumentarQuantidade: (id) => {
+
+        let qntdAtual = parseInt($("#qntd-" + id).text());
+        $("#qntd-" + id).text(qntdAtual + 1)
+
     }
+    
+
+
 }
 
 cardapio.templates = {
 
     item:`
-                        <div class="col-3 mb-5">
-                            <div class="card card-item">
-                                <div class="img-produto">
-                                    <img src="\${img}">
-                                </div>
-                                <p class="title-produto text-center mt-4">
-                                    <b>\${nome}</b>
-                                </p>
-                                <p class="price-produto text-center">
-                                    <b>R$ \${preco}</b>
-                                </p>
-                                <div class="add-carrinho">
-                                    <span class="btn-menos"><i class="fas fa-minus"></i></span>
-                                    <span class="add-numero-itens">0</span>
-                                    <span class="btn-mais"><i class="fas fa-plus"></i></span>
-                                    <span class="btn btn-add"><i class="fa fa-shopping-bag"></i></span>
-                                </div>
-                            </div>
+                <div class="col-3 mb-5">
+                    <div class="card card-item" id="produto-\${id}">
+                        <div class="img-produto">
+                            <img src="\${img}">
                         </div>
+                        <p class="title-produto text-center mt-4">
+                            <b>\${nome}</b>
+                        </p>
+                        <p class="price-produto text-center">
+                            <b>R$ \${preco}</b>
+                        </p>
+                        <div class="add-carrinho">
+                            <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidade('\${id}')"><i class="fas fa-minus"></i></span>
+                            <span class="add-numero-itens" id="qntd-\${id}">0</span>
+                            <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidade('\${id}')"><i class="fas fa-plus"></i></span>
+                            <span class="btn btn-add"><i class="fa fa-shopping-bag"></i></span>
+                        </div>
+                    </div>
+                </div>
+
     `
 
 }
